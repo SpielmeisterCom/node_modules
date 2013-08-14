83d9e715f727482a7b2220f887c3f92a2bf03b3f
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-08-14 15:08:27 +0200

    Added fsUtil node_module

---
+++ fsUtil/lib/index.js
@@ -0,0 +1,52 @@
+var fs     = require( 'fs' ),
+	os     = require( 'os' ),
+	path   = require( 'path' ),
+	wrench = require( 'wrench' ),
+	_      = require( 'underscore' )
+
+var copyFile = function( inputFilePath, outputFilePath ) {
+	fs.writeFileSync(
+		outputFilePath,
+		fs.readFileSync( inputFilePath )
+	)
+}
+
+/**
+ * Copies the specified files in filePaths to targetDirectoryPath - relative to the sourceDirectoryPath
+ *
+ * @param sourceDirectoryPath
+ * @param targetDirectoryPath
+ * @param filePaths
+ */
+var copyFiles = function( sourceDirectoryPath, targetDirectoryPath, filePaths ) {
+	_.each(
+		filePaths,
+		function( filePathInfo ) {
+			// indicates if the path and or name of the file changes from source to target location
+			var isMoved = _.isArray( filePathInfo ) && filePathInfo.length === 2
+
+			var sourceFilePath = isMoved ?
+				filePathInfo[ 0 ] :
+				path.resolve( sourceDirectoryPath,  filePathInfo )
+
+			if( isDirectory( sourceFilePath ) ) return
+
+			var targetFilePath = isMoved ?
+				filePathInfo[ 1 ] :
+				path.resolve( targetDirectoryPath,  filePathInfo )
+
+			var targetPath = path.dirname( targetFilePath )
+
+			if( !fs.existsSync( targetPath ) ) {
+				wrench.mkdirSyncRecursive( targetPath )
+			}
+
+			copyFile( sourceFilePath, targetFilePath )
+		}
+	)
+}
+
+module.exports = {
+	copyFiles : copyFiles,
+	copyFile : copyFile
+}
